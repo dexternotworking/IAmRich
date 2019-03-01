7ab0e78575e9c20cb572f09ae3fe215693224171
dexternotworking <47060719+dexternotworking@users.noreply.github.com>
2019-03-01 08:47:18 -0600

    Add files via upload

---
+++ I am Rich!/I am Rich!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich!
+//
+//  Created by MacAir11 on 02/27/19.
+//  Copyright © 2019 CCC iOS Boot Camp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
